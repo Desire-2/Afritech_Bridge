bcdad5c978d3323bd12c25df2e00bdac54e34a19
Desire-2 <bikorimanadesire5@gmail.com>
2025-10-18 16:14:40 +0200

    fix: remove static export to resolve 404 on Vercel

--- next.config.js
+++ next.config.js
@@ -6,15 +6,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   poweredByHeader: false,
 }
-
-module.exports = nextConfig
 
 module.exports = nextConfig
 
